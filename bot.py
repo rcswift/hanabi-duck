021e637372db9e6b088e05420c08ca1c0a0b467d
Ryan Swift <swiftryanc@gmail.com>
2022-02-08 22:37:03 -0500

    Created ClueBotMk3 which tries to do some cleverer things.
    Average score has improved to ~14

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 import logging
 
-from hanabi import Board, CardInformation, Clue, Discard, Play
+from operator import and_, not_, or_, eq
+
+from hanabi import Board, CardInformation, Clue, Discard, Play, CARD_NUMBERS, CARD_COLORS
 
 class BaseBot():
     def __init__(self, board: Board, index: int):
@@ -136,5 +138,70 @@ class ClueBotImproved(BaseBot):
         if self.board.clues >= 7:
             return Clue(target=self.board.relative_player(1), color="r") # throwaway clue. this is technically not allowed
         return Discard(self.board.my_hand_size - 1)
-
     
+class ClueBotMk3(BaseBot):
+    """More nuanced cluing and discarding"""
+    @property
+    def chop(self) -> int:
+        """Return the index of the highest un-clued card"""
+        non_clued_cards = [idx for idx, info in enumerate(self.board.current_information) if not info.clued]
+        if len(non_clued_cards) == 0:
+            chop = 0
+        else:
+            chop = max(non_clued_cards)
+        logging.debug(f"Discarding card from position {chop}")
+        return chop
+
+    def cards_touched(self, clue: Clue) -> List[bool]:
+        """Simulate a clue on a hand to check which cards are touched"""
+        hand = self.board.player_hand(clue.target)
+        if clue.number:
+            return [card.number == clue.number for card in hand]
+        elif clue.color:
+            return [card.color == clue.color for card in hand]
+
+    def play(self) -> Turn:
+        # Play clued cards
+        for i, card_info in enumerate(self.board.current_information):
+            # Play cards from right to left
+            if card_info.clued:
+                return Play(i)
+
+        # If out of clues, discard last card (card will never be clued because we play clued cards in prev step)
+        if self.board.clues == 0:
+            return Discard(self.chop)
+
+        good_clues = []
+
+        # Scan all possible clues:
+        for player_offset in range(1,self.board.num_players):
+            target = self.board.relative_player(player_offset)
+             # The hand that this potential clue would target
+            hand = self.board.player_hand(target)
+            # The cards that are immediately playable from this hand
+            playable = list(map(self.board.is_playable, hand))
+
+            should_touch = playable
+
+            possible_clues = [Clue(target, number=number) for number in set(CARD_NUMBERS)] + [Clue(target, color=color) for color in set(CARD_COLORS)]
+
+            for clue in possible_clues:
+                would_touch = self.cards_touched(clue)
+                num_touch = sum(would_touch)
+
+                # Check that this clue touches only playable cards
+                if sum(map(eq, should_touch, would_touch)) == self.board.cards_per_player and num_touch != 0:
+                    logging.debug(f"Found good clue: {clue}: {num_touch} cards")
+                    good_clues.append((clue, num_touch))
+
+
+        if len(good_clues) > 0:
+            # Now that we have a list of good clues (clues that touch only immediately playable cards), we pick which one 
+            # to give based on which player needs information the most:
+            good_clues.sort(key=lambda x: x[1])
+            return good_clues[0][0]
+        else:
+            # Otherwise discard last card
+            if self.board.clues >= 7:
+                return Clue(target=self.board.relative_player(1), color="r") # throwaway clue. this is technically not allowed
+            return Discard(self.chop)
